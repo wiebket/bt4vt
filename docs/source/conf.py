80dcb3ca6b728d2c8cf80ae5ccc243dd631bad7a
ut15upec <anna.leschanowsky@fau.de>
2023-01-23 22:21:10 +0100

    Update path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 import sys
 from pathlib import Path
 
-projroot = Path(__file__).resolve().parent.parent.parent # <this_file>/../../../
+projroot = Path(__file__).resolve().parent.parent # <this_file>/../../../
 sys.path.insert(0, str(projroot))
 
 # -- Project information -----------------------------------------------------
